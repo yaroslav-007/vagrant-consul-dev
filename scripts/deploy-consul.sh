0d9cf2ead01311275c251a7d34106b4304868b1b
Yaroslav <yorimark@abv.bg>
2019-08-14 13:04:28 +0300

    bliak2

--- scripts/deploy-consul.sh
+++ scripts/deploy-consul.sh
@@ -7,5 +7,7 @@ sudo rsync --chown=consul:consul --chmod=640  -og -I ./config/config.hcl  /etc/c
 ###(Re)starting the service
 sudo systemctl start consul
 
+
 ###Wait to service to power on in order for the next scripts to run successfuly
-sleep 240
+sleep 20
+sudo systemctl status consul
