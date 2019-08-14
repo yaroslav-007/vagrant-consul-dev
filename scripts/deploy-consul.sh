6151e6e975d27eed55933ac2dda94d36bcaa3794
Yaroslav <yorimark@abv.bg>
2019-08-14 12:35:19 +0300

    adding stuff

--- scripts/deploy-consul.sh
+++ scripts/deploy-consul.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -x
 [ -d /vagrant ] && pushd /vagrant
  
 sudo rsync --chown=consul:consul --chmod=640  -og -I ./config/config.hcl  /etc/consul.d/config.hcl
 
 ###(Re)starting the service
-sudo systemctl restart consul
+sudo systemctl start consul
 
 ###Wait to service to power on in order for the next scripts to run successfuly
-sleep 15
+sleep 240
